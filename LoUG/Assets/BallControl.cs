91ea16fb283200d2d176bfafa2f8ca5f4dedff32
Logan Smith <lmaxsmith@gmail.com>
2019-01-03 07:52:13 -0800

    Ported to Magic Leap. Updated ML package and SDK. Setup ML controller to select, grab, move, and instantiate balls. Move is a spring-like force tug. Something in the regular gravity went weird and it's super strong.

---
+++ LoUG/Assets/BallControl.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class BallControl : MonoBehaviour {
+
+    #region Variabls
+    //public 
+    public GameObject ballPrefab;
+
+    //private 
+    Gravity gravityGroup;
+    public GameObject selectedBall;
+    public GameObject grabbedBall;
+    //constants
+    const float grabStrength = 2;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        gravityGroup = FindObjectOfType<Gravity>();
+
+        //subscribe methods for controller
+        MLInput.OnTriggerDown += HandleTriggerDown;
+        MLInput.OnTriggerUp += HandleTriggerUp;
+    }
+    //unsubscribe from events
+    private void OnDestroy()
+    {
+        MLInput.OnTriggerDown -= HandleTriggerDown;
+        MLInput.OnTriggerUp -= HandleTriggerUp;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == gravityGroup.gameObject.layer)
+        {
+            selectedBall = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        selectedBall = null;
+    }
+
+    //what to do when trigger is pressed
+    void HandleTriggerDown(byte controllerId, float value)
+    {
+        if (selectedBall)
+        {
+            grabbedBall = selectedBall;
+            StartCoroutine("MoveBall");
+        }
+        else
+        {
+            NewBall();
+        }
+    }
+
+    //what to do when trigger is released
+    void HandleTriggerUp(byte controllerId, float value)
+    {
+        grabbedBall = null;
+        StopAllCoroutines();
+    }
+
+
+    void NewBall()
+    {
+        GameObject newBall = Instantiate(ballPrefab,gravityGroup.transform);
+        newBall.transform.position = transform.position;
+        newBall.GetComponent<Rigidbody>().mass = .01f;
+        grabbedBall = newBall;
+        StartCoroutine("InflateBall");
+        StartCoroutine("MoveBall");
+    }
+
+    IEnumerator MoveBall()
+    {
+        while (true)
+        {
+            // Gets a vector that points from the this ball to the other.
+            var heading = transform.position - grabbedBall.transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance; // This is now the normalized direction.
+
+            //calculate pull to other ball (in normalized direction) based on mass and distance)
+            Vector3 relativePull = Vector3.zero;
+            relativePull.x = Mathf.Clamp(direction.x * grabbedBall.GetComponent<Rigidbody>().mass * grabStrength * gravityGroup.gravitationalConstant * distance * distance, -gravityGroup.forceclamp, gravityGroup.forceclamp);
+            relativePull.y = Mathf.Clamp(direction.y * grabbedBall.GetComponent<Rigidbody>().mass * grabStrength * gravityGroup.gravitationalConstant * distance * distance, -gravityGroup.forceclamp, gravityGroup.forceclamp);
+            relativePull.z = Mathf.Clamp(direction.z * grabbedBall.GetComponent<Rigidbody>().mass * grabStrength * gravityGroup.gravitationalConstant * distance * distance, -gravityGroup.forceclamp, gravityGroup.forceclamp);
+            grabbedBall.GetComponent<Rigidbody>().AddForce(relativePull);
+
+            yield return null; 
+        }
+    }
+
+    IEnumerator InflateBall()
+    {
+        while (true)
+        {
+            grabbedBall.GetComponent<Rigidbody>().mass += .01f;
+            yield return null;
+        }
+    }
+
+}
---
+++ LoUG/Assets/Gravity.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+using MagicLeap;
+
+public class Gravity : MonoBehaviour {
+
+    #region Variables
+    //public Variabls
+    public float gravitationalConstant = 1f;
+    public float forceclamp = 100;
+
+    //private variables
+    public Rigidbody[] gravityBallz;
+
+    //constants
+    
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        //Gather and size the balls
+        gravityBallz = gameObject.GetComponentsInChildren<Rigidbody>();
+
+        //cycle through each ball
+        for (int i = 0; i < gravityBallz.Length; i++)
+        {
+            //adjust ball size according to rigid body mass
+            gravityBallz[i].transform.localScale = new Vector3(
+                Mathf.Pow(gravityBallz[i].mass, 1f / 3f), Mathf.Pow(gravityBallz[i].mass, 1f / 3f), Mathf.Pow(gravityBallz[i].mass, 1f / 3f));
+
+
+            ApplyGravity(gravityBallz[i]);
+            
+        }
+	}
+
+    
+
+    void ApplyGravity(Rigidbody gravityBall)
+    {
+        //cycle through each other ball to calculate pull
+        for (int j = 0; j < gravityBallz.Length; j++)
+        {
+            // Gets a vector that points from the this ball to the other.
+            var heading = gravityBall.transform.position - gravityBallz[j].transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance; // This is now the normalized direction.
+
+            //calculate pull to other ball (in normalized direction) based on mass and distance)
+            Vector3 relativePull = Vector3.zero;
+            relativePull.x = Mathf.Clamp(direction.x * gravityBall.mass * gravityBallz[j].mass * gravitationalConstant / distance / distance, -forceclamp, forceclamp);
+            relativePull.y = Mathf.Clamp(direction.y * gravityBall.mass * gravityBallz[j].mass * gravitationalConstant / distance / distance, -forceclamp, forceclamp);
+            relativePull.z = Mathf.Clamp(direction.z * gravityBall.mass * gravityBallz[j].mass * gravitationalConstant / distance / distance, -forceclamp, forceclamp);
+            gravityBallz[j].AddForce(relativePull,ForceMode.Force);
+            
+        }
+    }
+
+    
+}
